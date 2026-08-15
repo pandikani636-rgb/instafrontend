ee5f3d4f5bc08154b94ada45ed99723789c419fd
pandikani <pandikani636@gmail.com>
2026-08-15 19:38:29 +0530

    Revert "jnjdnjfd"
    This reverts commit c0a3e92d1c636d83f1ec0605c5d6c41e1bc1c6d1.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -55,7 +55,7 @@ const LoginForm = () => {
         />
 
         <InputField
-          label="Confirmpassword"
+          label="Role"
           type="text"
           name="role"
           value={formData.role}
